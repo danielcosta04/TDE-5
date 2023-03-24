57929e921d883aba011965e9967aced3c1954fc9
Daniel Costa <124638683+danielcosta04@users.noreply.github.com>
2023-03-24 23:15:33 +0000

    Initial commit

---
+++ tde-5.js
@@ -0,0 +1,70 @@
+2
+  function next(){
+  document.write(`<br>`)
+}
+// document.write(`<br>1. Escreva uma função que retorne o cubo de um número fornecido pelo usuário.`)
+// next()
+// n = Number(prompt('Digite o numero: '))
+// function cbrt(n){
+//   return Math.cbrt(n);
+// }
+// next()
+// document.write(`<br>O número digitado foi: ${n}`)
+// document.write(`<br>A raiz cúbica: ${cbrt(n)}`)
+// next()
+// document.write(`<br>2. Escreva uma função que retorne a área de um triângulo, a partir dos valores de base.`)
+
+// next()
+// b = +(prompt('digite a base do triângulo: '))
+// h = +(prompt('Digite a altura do triângulo: '))
+// function triagle(b,h){
+//   return (b * h) / 2;
+// }
+// document.write(`<br>A base do triângulo é igual: ${b}`)
+// document.write(`<br>A altura do triângulo é igual: ${h}`)
+// document.write(`<br>A area do triângulo é igual: ${triagle(b,h)}`)
+
+//next()
+
+// document.write(`<br>3. Escreva uma função que informe o retorno de um investimento (montante) com base nos valores do capital inicial, tempo em meses e taxa de juros mensal, fornecidos pelo usuário. 
+// Use a fórmula: M = C * (1+i)t
+// <br>
+// Onde:
+// <br>
+// ■ C = Capital inicial investido<br>
+// ■ i = Taxa de juros, em percentual<br>
+// ■ t = Tempo do investimento, em meses<br>
+// Exiba o resultado com duas casas decimais.`)
+// next()
+// c = +(prompt('Digite o montante do capital inicial ivenstido: '))
+// i = +(prompt('Digite taxa de juros ao mês: '))
+// t = +(prompt('Digite o tempo em meses do Capital investido: '))
+
+// function invest(c,i,t){
+//   return c * (1 + i) * t;
+// }
+// next()
+// document.write(`<br>C = Capital inicial investido: R$ ${c.toFixed(2)}`)
+// document.write(`<br>i = Taxa de juros, em percentual: ${i.toFixed(2)} %`)
+// document.write(`<br>t = Taxa de juros, em percentual: ${t} meses`)
+// document.write(`<br>O retorno do capital investido é: ${invest(c,i,t).toFixed(2)}`)
+
+// next()
+// document.write(`<br>4. Criar uma função que junte dois arrays e retorno o resultado como um novo array`)
+
+// next()
+
+// viagem = ['Hotel', 'Passeios', 'Feirinha', 'Jantar Romântico']
+// adicionais = ['Passagem aérea', 'Translado', 'Cotação da moeda','Guia turistico']
+
+// ferias = viagem.concat(adicionais).sort().join(' <br> ')
+
+// document.write(`<br>No planejamento das férias temos que colocar os seguintes itens:<br> ${ferias}`)
+
+next()
+document.write(`<br>5. Escreva um programa que receba dois números reais e um código de seleção do usuário. 
+Se o código digitado for 1, faça o algoritmo adicionar os dois números previamente digitados e mostrar o resultado; 
+se o código de seleção for 2, os números deverão ser multiplicados; 
+se o código de seleção for 3, o primeiro número deve ser dividido pelo segundo.`)
+
+
